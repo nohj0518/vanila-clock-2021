5a358db9827212e4f31a15eb12175e482264c9fe
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-04-19 00:42:30 +0900

    Save User Name

--- greeting.js
+++ greeting.js
@@ -4,6 +4,23 @@ const form = document.querySelector(".js-form"),
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,6 +31,7 @@ function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
     //user is not
+    askForName();
   } else {
     //user is
     paintGreeting(currentUser);
